45b681086c5d93a90e269e3dedfa67cc84d2e729
apsa <n.Lohrer@campus.lmu.de>
2023-11-25 01:18:26 +0100

    Initial commit

---
+++ PracticeTrackerAPI/Controllers/SessionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeTracker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SessionController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
